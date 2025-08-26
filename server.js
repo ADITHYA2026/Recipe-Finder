2e53c5d4046760919808bbd2025a521b58ec56be
ADITHYA PRATHI <adithyakumarprathi@gmail.com>
2025-08-26 13:50:36 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 import recipesRouter from "./routes/recipes.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
